0399af17b121328bd05570f3d7f0d8bd65b05d03
Francois-Xavier <fxbabin@outlook.fr>
2020-09-26 21:23:58 +0200

    feat: finished networking part

--- 01_networking.tf
+++ 01_networking.tf
@@ -43,7 +43,7 @@ resource "aws_subnet" "cloud1_private_subnet" {
   depends_on = [aws_vpc.cloud1_vpc]
 
   vpc_id                  = aws_vpc.cloud1_vpc.id
-  count                   = length(var.availability_zones) - 1 
+  count                   = length(var.availability_zones) - 1
   availability_zone       = var.availability_zones[count.index]
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 4)
   map_public_ip_on_launch = false
@@ -52,4 +52,82 @@ resource "aws_subnet" "cloud1_private_subnet" {
     "Name"    = "${var.project_name}-priv-${count.index + 1}"
     "Project" = var.project_name
   }
+}
+
+###########
+#   IGW   #
+###########
+
+resource "aws_internet_gateway" "cloud1_igw" {
+  depends_on = [aws_vpc.cloud1_vpc]
+  vpc_id     = aws_vpc.cloud1_vpc.id
+
+  tags = {
+    "Name"    = "${var.project_name}-igw"
+    "Project" = var.project_name
+  }
+}
+
+###################
+#   ROUTE TABLE   #
+###################
+
+resource "aws_route_table" "cloud1_public_rt" {
+  depends_on = [
+    aws_vpc.cloud1_vpc,
+    aws_internet_gateway.cloud1_igw
+  ]
+
+  vpc_id = aws_vpc.cloud1_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cloud1_igw.id
+  }
+
+  tags = {
+    "Name"    = "${var.project_name}-pub-rt"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_route_table" "cloud1_private_rt" {
+  depends_on = [
+    aws_vpc.cloud1_vpc,
+    aws_internet_gateway.cloud1_igw
+  ]
+
+  vpc_id = aws_vpc.cloud1_vpc.id
+
+  tags = {
+    "Name"    = "${var.project_name}-priv-rt"
+    "Project" = var.project_name
+  }
+}
+
+#   Route table association
+##############################
+
+resource "aws_route_table_association" "cloud1_public_rt_assoc" {
+  depends_on = [
+    aws_route_table.cloud1_public_rt,
+    aws_subnet.cloud1_public_subnet
+  ]
+
+  count     = length(aws_subnet.cloud1_public_subnet)
+  subnet_id = aws_subnet.cloud1_public_subnet[count.index].id
+
+  route_table_id = aws_route_table.cloud1_public_rt.id
+}
+
+resource "aws_route_table_association" "cloud1_private_rt_assoc" {
+  depends_on = [
+    aws_route_table.cloud1_private_rt,
+    aws_subnet.cloud1_private_subnet
+  ]
+
+  count     = length(aws_subnet.cloud1_private_subnet)
+  subnet_id = aws_subnet.cloud1_private_subnet[count.index].id
+
+  route_table_id = aws_route_table.cloud1_private_rt.id
 }
